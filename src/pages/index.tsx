4c71713288943a05120249e90b6470de17e41972
Diogo Silva <diogo@MacBook-Pro-de-Diogo.local>
2023-04-16 15:46:52 -0300

    Produto & Checkout - Estrutura da página de produto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,13 @@
 import { HomeContainer, Product } from "../styles/pages/home";
+import Image from 'next/image';
+import Link from 'next/link';
 
 import { useKeenSlider } from 'keen-slider/react';
 
-import Image from 'next/image';
+import { stripe } from "../lib/stripe";
+import { GetStaticProps } from "next";
 
 import 'keen-slider/keen-slider.min.css';
-
-import { stripe } from "../lib/stripe";
-
-import { GetStaticProps } from "next";
 import Stripe from "stripe";
 
 interface HomeProps {
@@ -33,7 +32,8 @@ export default function Home({ products }: HomeProps) {
 
       {products.map(product => {
         return (
-          <Product key={product.id} className="keen-slider__slide">
+          <Link href={`/product/${product.id}`} key={product.id}>
+          <Product className="keen-slider__slide">
             <Image src={product.imageUrl} width={520} height={480} alt="" />
 
             <footer>
@@ -41,6 +41,7 @@ export default function Home({ products }: HomeProps) {
               <span>{product.price}</span>
             </footer>
           </Product>
+          </Link>
         )
       })}
 
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,24 @@
+
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
 import { useRouter } from "next/router"
 
 export default function Product() {
   const {query} = useRouter()
 
   return (
-    <h1>Product {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 69,90</span>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam reprehenderit quia voluptatibus alias eum dolorem incidunt! Atque porro veritatis expedita sit fugiat repellendus beatae numquam, ipsa, at voluptatem ab consequuntur.</p>
+
+        <button>
+          Comprar Agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
